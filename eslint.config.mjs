9b12a9e5d5ce66e7ecdcf7c7bf42c4d3d9ace7a7
tea tang <534427931@qq.com>
2026-01-22 21:33:38 +0800

    update config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -36,5 +36,10 @@ export default defineConfig(
       ]
     }
   },
-  eslintConfigPrettier
+  eslintConfigPrettier,
+  {
+    rules: {
+      'prettier/prettier': ['error', { endOfLine: 'auto' }]
+    }
+  }
 )
