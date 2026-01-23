d261a7bf53d30a9735791a5af005b3af9a69e723
唐彬峰 <534427931@qq.com>
2026-01-23 23:55:01 +0800

    Release version 0.2.2

--- src/renderer/src/utils/index.ts
+++ src/renderer/src/utils/index.ts
@@ -15,18 +15,3 @@ export {
   SQL_MULTI_WORD_KEYWORDS,
   SQL_LINE_START_KEYWORDS
 } from './stringTools'
-
-// Tetris Core
-export {
-  BOARD_WIDTH,
-  BOARD_HEIGHT,
-  SHAPES,
-  TETROMINOES,
-  type TetrominoShape,
-  getPieceMatrix,
-  collision,
-  createEmptyBoard,
-  clearLines,
-  calculateDropDistance,
-  generateRandomShape
-} from './tetrisCore'
